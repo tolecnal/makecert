309cad9d227f1dddbf615cf733f262aa53087435
Jostein Elvaker Haande <tolecnal@tolecnal.net>
2011-11-05 21:51:45 +0100

    Cleaned up some code. Added error trapping to when running the openssl binary.

--- makecert.sh
+++ makecert.sh
@@ -3,77 +3,91 @@
 # Script to generate self signed certificates for use
 # with Apache2 based web servers.
 
-BIN=/usr/bin/openssl
+BIN="/usr/bin/openssl"
 
-DEF_USER="Jostein Elvaker Haande"
-DEF_MAIL="tolecnal@tolecnal.net"
-DEF_ORG="Xiro Networks"
+DEF_USER="John Average Doe"
+DEF_MAIL="user@domain.tld"
+DEF_ORG="Acme Networks"
 DEF_DOMAIN="sub.domain.tld"
 DEF_COUNTRY="NO"
-DEF_STATE="Oslo"
-DEF_LOCATION="Norway"
+DEF_STATE="State"
+DEF_LOCATION="Country"
+
+DEBUG=0
 
 echo -e "\033[1mChecking for openssl binary\033[0m"
-if [ ! -f $BIN ]; then
+if [ ! -f "$BIN" ]; then
     echo "WARNING! openssl binary not found... WARNING!"
-    exit 0
+    exit 1
 fi
 
 echo -e "\033[1mEnter administrative name (ie: John Doe)\033[0m"
-read USER
+read -e USER
 if [ -z "$USER" ]; then
     echo "No name entered, using default ($DEF_USER)"
-    USER=$DEF_USER
+    USER="$DEF_USER"
 fi
 
 echo -e "\033[1mEnter adminstrative e-mail address (ie: user@domain.tld)\033[0m"
-read EMAIL
+read -e EMAIL
 if [ -z "$EMAIL" ]; then
     echo "No e-mail address entered, using default ($DEF_MAIL)"
-    EMAIL=$DEF_MAIL
+    EMAIL="$DEF_MAIL"
 fi
 
 echo -e "\033[1mEnter organization name (ie: Acme Networks Ltd)\033[0m"
-read ORG
+read -e ORG
 if [ -z "$ORG" ]; then
     echo "No organization entered, using default ($DEF_ORG)"
-    ORG=$DEF_ORG
+    ORG="$DEF_ORG"
 fi
 
 echo -e "\033[1mEnter domain name (ie: sub.domain.tld)\033[0m"
-read DOMAIN
+read -e DOMAIN
 if [ -z "$DOMAIN" ]; then
     echo "No domain entered, using default ($DEF_DOMAIN)"
-    DOMAIN=$DEF_DOMAIN
+    DOMAIN="$DEF_DOMAIN"
 fi
 
 echo -e "\033[1mEnter country (ie: US)\033[0m"
-read COUNTRY
+read -e COUNTRY
 if [ -z "$COUNTRY" ]; then
     echo "No country entered, using default ($DEF_COUNTRY)"
-    COUNTRY=$DEF_COUNTRY
+    COUNTRY="$DEF_COUNTRY"
 fi
 
 echo -e "\033[1mEnter state/city (ie: New York)\033[0m"
-read STATE
+read -e STATE
 if [ -z "$STATE" ]; then
     echo "No state entered, using default ($DEF_STATE)"
-    STATE=$DEF_STATE
+    STATE="$DEF_STATE"
 fi
 
 echo -e "\033[1mEnter location (ie: Manhatten)\033[0m"
-read LOCATION
+read -e LOCATION
 if [ -z "$LOCATION" ]; then
     echo "No location entered, using default ($DEF_LOCATION)"
-    LOCATION=$DEF_LOCATION
+    LOCATION="$DEF_LOCATION"
 fi
 
 OPTS=(/C="$COUNTRY"/ST="$STATE"/L="$LOCATION"/O="$ORG"/OU="$DOMAIN"/CN="$USER"/emailAddress="$EMAIL")
-echo "${OPTS[@]}"
+
+if [ $DEBUG ] ; then
+    echo "${OPTS[@]}"
+fi
 
 COMMAND=($BIN req -new -x509 -nodes -days 365 -subj "${OPTS[@]}" -newkey rsa:1024 -keyout $DOMAIN.key -out $DOMAIN.cert)
 
-echo "Running ${COMMAND[@]}"
-"${COMMAND[@]}"
+if [ $DEBUG ] ; then
+    echo "Running ${COMMAND[@]}"
+fi
 
-echo "Done!"
+"${COMMAND[@]}"
+if (( $? )) ; then
+    echo -e "ERROR: Something went wrong!"
+    exit 1
+else
+   echo "Done!"
+fi
+
+exit 0
